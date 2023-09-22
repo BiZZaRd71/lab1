eafcff4f8247b57c54412aa6bd026ebab3779d09
Богдан Дмитриев <111640301+BiZZaRd71@users.noreply.github.com>
2023-09-22 20:52:20 +0300

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,2 +1,10 @@
 print("Лабараторна1")
 print("Богдан Дмітрієв КІ1")
+try:
+    a = int (input("Введіть змінну а"))
+    b = int (input("Введіть змінну b"))
+    c = int (input("Введіть змінну c"))
+    d = int (input("Введіть змінну d"))
+    print(a + b / c ** d)
+except:
+    print("syntax error")
